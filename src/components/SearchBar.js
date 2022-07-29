73810bf151f3d9e0582e08469a5800f257dfb175
mehdi <user_email@mail.com>
2022-07-29 12:59:14 +0430

    feat: add new SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,43 @@
+import { useContext, useEffect, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-import { VscSearch } from 'react-icons/vsc';
+import { VscSearch } from "react-icons/vsc";
+
 const SearchBar = () => {
-    return (
-        <div className="flex justify-center items-center">
+  const inputRef = useRef();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const searchTerm = location.search.substring(3);
 
-            <input type="text" placeholder="Search youtube" className="input input-bordered lg:w-1/3 rounded-none border-gray-400 focus:outline-none" />
+  useEffect(() => {
+    inputRef.current.value = searchTerm;
+  }, [searchTerm]);
 
-            <button class="btn btn-ghost rounded-none border-gray-400 bg-gray-200">
-                <VscSearch className='text-2xl ' style={{ transform: "rotateY(180deg)" }} />
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/search?q=${encodeURIComponent(inputRef.current.value)}`);
+  };
 
-            </button>
-        </div>
+  return (
+    <form
+      className="flex justify-center items-center w-full"
+      onSubmit={onFormSubmit}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Search youtube"
+        className="input input-bordered lg:w-1/3 rounded-none border-gray-400 focus:outline-none"
+      />
 
-
-
-    )
-}
+      <button className="btn btn-ghost rounded-none border-gray-400 bg-gray-200">
+        <VscSearch
+          className="text-2xl "
+          style={{ transform: "rotateY(180deg)" }}
+        />
+      </button>
+    </form>
+  );
+};
 
 export default SearchBar;
