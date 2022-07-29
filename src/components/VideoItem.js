383977c990070c24942c879bab3d527f0ac44b56
mehdi <user_email@mail.com>
2022-07-30 02:24:24 +0430

    feat: <Videoltem> ui

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,21 @@
 const VideoItem = ({ snippet: { title, thumbnails, channelTitle } }) => {
   return (
-    <div className="flex gap-x-2">
-      <img src={thumbnails.default.url} alt="video-youtube" />
-      <div className="flex flex-col gap-y-3">
-        <p>{title}</p>
-        <strong>{channelTitle}</strong>
+    <div className="flex flex-col gap-y-4 items-start">
+      <img
+        className="w-full aspect-video"
+        src={thumbnails.high.url}
+        alt={title}
+      />
+      <div className="flex text-sm items-start gap-x-4">
+        <img
+          className="rounded-full aspect-square"
+          src="https://picsum.photos/50/50"
+          alt=""
+        />
+        <div className="block">
+          <strong>{channelTitle}</strong>
+          <p>{title}</p>
+        </div>
       </div>
     </div>
   );
