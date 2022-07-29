bb0e9da6ae22a0cd3029008b3d9290fa0238aefc
mehdi <user_email@mail.com>
2022-07-29 19:56:36 +0430

    feat: <NavBar> ui

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,21 @@
+import { VscBell, VscDeviceCameraVideo } from "react-icons/vsc";
+
 import SearchBar from "./SearchBar";
+import youtubeLogo from "../assets/youtube-logo.png";
 
 const NavBar = () => {
-  return <SearchBar />;
+  return (
+    <div className="flex justify-between py-2 px-10 border-b-2">
+      <img className="w-24" src={youtubeLogo} alt="YouTubeLogo" />
+
+      <SearchBar />
+
+      <div className="flex justify-between items-center  gap-x-6">
+        <VscDeviceCameraVideo className="text-2xl" />
+        <VscBell className="text-2xl" />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
