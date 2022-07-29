cad00d00808a7a271d772152b0e6c3182fb017b1
mehdi <user_email@mail.com>
2022-07-29 12:46:19 +0430

    feat: add api

---
+++ src/utils/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+class Api {
+  axios;
+
+  constructor() {
+    this.axios = axios.create({
+      baseURL: "https://www.googleapis.com/youtube/v3",
+      params: {
+        part: "snippet",
+        key: process.env.REACT_APP_API_KEY,
+      },
+    });
+  }
+
+  async search(term) {
+    try {
+      const { status, data } = await this.axios.get("search", {
+        params: { q: term, maxResults: 5 },
+      });
+
+      if (status !== 200)
+        return console.warn("search request error:" + status, data);
+      return data.items;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default new Api();
