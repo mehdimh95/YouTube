e206d1423b06de0e9413bb39ebec5f5b888d0a64
mehdi <user_email@mail.com>
2022-07-31 03:50:07 +0430

    feat: add <App>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import Video from "./pages/Video";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="search" element={<Search />} />
+      <Route path="v">
+        <Route path=":id" element={<Video />} />
+      </Route>
     </Routes>
   );
 };
