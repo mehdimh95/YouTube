ba2409d1491ee3913aad593a0c53cb611bf8ebdb
mehdi <user_email@mail.com>
2022-07-30 15:48:57 +0430

    feat: use context instead state

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,19 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 
 import NavBar from "../components/NavBar";
+import VideoContext from "../VideoContext";
 import VideoList from "../components/VideoList";
 import api from "../utils/api";
 import { useLocation } from "react-router-dom";
 
 const Search = () => {
-  const [videos, setVideos] = useState([]);
+  const { videos, setVideos } = useContext(VideoContext);
   const location = useLocation();
   const searchTerm = location.search.substring(3);
 
   useEffect(() => {
     const getResult = async () => {
-      const result = await api.search(searchTerm || "cat");
+      const result = await api.search(searchTerm);
       setVideos(result);
     };
     getResult();
