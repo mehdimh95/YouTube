ea782400ed5807af79aef2ff8380952801298871
mehdi <user_email@mail.com>
2022-07-31 14:41:31 +0430

    fix: <videolist>

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ const VideoList = ({ videos }) => {
   // console.log(videos);
   return (
     <div className="grid grid-cols-4 gap-x-4 gap-y-10 p-10 border-t-2">
-      {videos.length
+      {videos && videos.length
         ? videos.map((video) => (
             <div key={video.id.videoId}>
               <VideoItem {...video} />
